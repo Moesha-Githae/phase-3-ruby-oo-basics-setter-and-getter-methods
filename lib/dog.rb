0e1aee5d376a68ee4dad4f2f53eeada773260acf
Moesha-Githae <moesha.githae@student.moringaschool.com>
2023-05-21 12:14:32 +0300

    complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
+ def name =(dog_name)
+    @dog_name = dog_name
+ end 
+ def name
+    @dog_name
+ end 
+
+ def breed = (breed)
+    @breed = breed
+ end 
+ def breed 
+    @breed
+ end
 
 end
